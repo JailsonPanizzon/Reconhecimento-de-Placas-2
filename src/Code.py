3c77e8d73d9c057aa01785a396553ca2df24e6bb
Jailson Panizzon <jailsonpanizzon@alunos.utfpr.edu.br>
2019-06-18 15:42:58 -0300

    codigo do classificador

---
+++ src/Code.py
@@ -0,0 +1,14 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+cascade = cv2.CascadeClassifier("cascade.xml")
+while True:
+    ret, img = cap.read()
+    placa = cascade.detectMultiScale(img, 1.9,5)
+    for (x,y,w,h) in placa:
+        cv2.rectangle(img, (x,y),(x+w,y+h),(255,0,0),2)
+
+    cv2.imshow("shown",img)
+    k=cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
