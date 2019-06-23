59e9f4c02c79b1690953a0b82a312fc1d880eca3
Jailson Panizzon <jailsonpanizzon@alunos.utfpr.edu.br>
2019-06-23 20:59:26 -0300

    detecção de cor pronto

--- src/Code.py
+++ src/Code.py
@@ -5,15 +5,23 @@ cap = cv2.VideoCapture("VID-20190617-WA0009.mp4")
 cascade = cv2.CascadeClassifier("cascade.xml")
 while True:
     ret, img = cap.read()
+    cor1 =[255,100,50]
+    cor2 =[50,0,0]
+    #cor1 = cl.convert2hsv([50,255,50])
+    #cor2.append(cor1[0]-1)
+    #cor2.append(cor1[1]-1)
+    #cor2.append(20)
 
-    cord = cl.detect(img,[10,10,230])
-    print(cord)
-    if(cord[0] == 0 and cord[1] == 0 and cord[2] == 0 and cord[3] == 0):
+    cord = cl.detect(img,cor1,cor2)
+    print(cor1)
+    print(cor2)
+    if (not(cord[0] == 0 and cord[1] == 0 and cord[2] == 0 and cord[3] == 0)):
+        img = img[cord[1]:cord[1]+cord[3],cord[0]:cord[0]+cord[2]]
         placa = cascade.detectMultiScale(img, 1.9,5)
+        print(cord)
         for (x,y,w,h) in placa:
             cv2.rectangle(img, (x,y),(x+w,y+h),(255,0,0),2)
-
-    cv2.imshow("shown",img)
+        cv2.imshow("shown",img)
     k=cv2.waitKey(30) & 0xff
     if k == 27:
         break
--- src/detect_color.py
+++ src/detect_color.py
@@ -1,14 +1,41 @@
 import cv2
 import numpy as np
+from PIL import Image
+import colorsys
 
 class cl:
-    def detect(img,cor):
+    def detect(img,cor1,cor2):
+        '''im2 = Image.new('RGB',(280,280),(255,255,255))
+        im2.save('img.png')
+        im = cv2.imread('img.png')
+        im2 =cv2.imread('img.png')
+        i=0
+        while(i<279):
+            j=0
+            while(j<279):
+                im[i][j] = cor1
+                im2[i][j]=cor2
+                j+=1
+            i+=1
+        cv2.imwrite("imgfg.png",im2)
+        cv2.imwrite("img.png",img)'''
         kernel = np.ones((5 , 5), np.uint8)
         cord = []
-        val = 5
-        rangomax = np.array([int(cor[0])+val,int(cor[1])+val,int(cor[2])+val])
-        rangomin = np.array([int(cor[2])-val,int(cor[2])-val,int(cor[2])-val])
+        rangomax = np.array([int(cor1[2]),int(cor1[1]),int(cor1[0])])
+        rangomin = np.array([int(cor2[2]),int(cor2[1]),int(cor2[0])])
         mask = cv2.inRange(img,rangomin,rangomax)
         opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
         cord = cv2.boundingRect(opening)
+        x, y, w, h = cv2.boundingRect(opening)
+        print(x, y, w, h)
+        cv2.rectangle(img, (x, y), (x+w, y + h), (255, 255, 0), 5)
+        cv2.imshow("cor", mask)
         return cord
+
+    def convert2hsv(rgb):
+        hsv=colorsys.rgb_to_hsv(rgb[0],rgb[1],rgb[2])
+        return hsv
+
+    def convert2rgb(hsv):
+        rgb = colorsys.hsv_to_rgb(hsv[0],hsv[1],hsv[2])
+        return rgb
