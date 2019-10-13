2efcec61f1cf22d08efa194c6c0a2d6b395a6298
JailsonPanizzon <jailson.panizzon@gmail.com>
2019-10-13 00:12:46 -0300

    Dat set limpo(denovo) e experimento rodado

--- src/Code_images.py
+++ src/Code_images.py
@@ -1,24 +1,33 @@
+# Autor Jailson Lucas Panizzon, jailson.panizzon@gmail.com
+# Problema com diretorio podem estar relacionado com a função os.listdir
+# Use python 3.7 para evitar problemas
 import cv2
 import os
 from detect_color import cl
 
 
-cascade = cv2.CascadeClassifier("cascade.xml")
+cascade = cv2.CascadeClassifier("src/cascade.xml")
 cont = 0
-contp=0
-for i in os.listdir("avaliacao"):
-    img = cv2.imread("avaliacao/"+str(i)) 
-    cor1 =[255,200,200]
-    cor2 =[60,0,0]
+total = 0
+tipo = "/positivas"
+#tipo = "/negativas"
+contp = 0
+for i in os.listdir("src/avaliacao"+tipo):
+    total += 1
+    img = cv2.imread("src/avaliacao"+tipo+"/"+str(i))
+    print(total)
+    cor1 = [255, 200, 200]
+    cor2 = [60, 0, 0]
     img = cl.equaliza(img)
     #img = cl.detect(img,cor1,cor2)
-    tam = (int(img.shape[0]*0.5),int(img.shape[1]*0.2))
-    placa = cascade.detectMultiScale(img,1.1,5,0,tam)
-    
-    print("========================================")    
-    for (x,y,w,h) in placa:
-        cv2.rectangle(img, (x,y),(x+w,y+h),(255,0,0),1)
-        yh,yh2,xh,xh2 = y,y+h,x,x+w
+    tam = (int(img.shape[0]*0.5), int(img.shape[1]*0.2))
+    placa = cascade.detectMultiScale(img, 1.1, 30, 0, tam)
+
+    print("========================================")
+    find = False
+    for (x, y, w, h) in placa:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 1)
+        yh, yh2, xh, xh2 = y, y+h, x, x+w
         p = 0.5
         if(y-(h*p) > 0):
             yh = int(y-(h*p))
@@ -28,15 +37,20 @@ for i in os.listdir("avaliacao"):
             xh = int(x-(w*p))
         if(x+(w*p) < img.shape[1]-1):
             xh2 = int(x+w+(w*p))
-        img = img[yh:yh2,xh:xh2]
-        if(img.shape[0]>0 and img.shape[1] > 0 ):
-            cv2.imshow("shown",img)
-            contp+=1
-            cont+=1
-            cv2.imwrite("results/images/"+str(cont)+".png",img)
-            text = (cl.readText(img))
-            os.system("espeak -vpt '"+text+"'")
-            print(text)
-    k=cv2.waitKey(30) & 0xff
+        img = img[yh:yh2, xh:xh2]
+        find = True
+        # if(img.shape[0] > 0 and img.shape[1] > 0):
+        #cv2.imshow("shown", img)
+        #contp += 1
+        #cv2.imwrite("src/results/images"+tipo+"/"+str(cont)+".png", img)
+        #text = (cl.readText(img))
+        #os.system("espeak -vpt '"+text+"'")
+        # print(text)
+    if(find):
+        cont += 1
+    k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+
+print("Encontradas = "+str(cont))
+print("\nTotal = " + str(total))
--- src/detect_color.py
+++ src/detect_color.py
@@ -6,7 +6,7 @@ import pytesseract
 
 
 class cl:
-    def detect(img,cor1,cor2):
+    def detect(img, cor1, cor2):
         '''im2 = Image.new('RGB',(280,280),(255,255,255))
         im2.save('img.png')
         im = cv2.imread('img.png')
@@ -21,11 +21,11 @@ class cl:
             i+=1
         cv2.imwrite("imgfg.png",im2)
         cv2.imwrite("img.png",img)'''
-        kernel = np.ones((5 , 5), np.uint8)
+        kernel = np.ones((5, 5), np.uint8)
         cord = []
-        rangomax = np.array([int(cor1[2]),int(cor1[1]),int(cor1[0])])
-        rangomin = np.array([int(cor2[2]),int(cor2[1]),int(cor2[0])])
-        mask = cv2.inRange(img,rangomin,rangomax)
+        rangomax = np.array([int(cor1[2]), int(cor1[1]), int(cor1[0])])
+        rangomin = np.array([int(cor2[2]), int(cor2[1]), int(cor2[0])])
+        mask = cv2.inRange(img, rangomin, rangomax)
         opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
         cord = cv2.boundingRect(opening)
         x, y, w, h = cv2.boundingRect(opening)
@@ -41,14 +41,14 @@ class cl:
         return rgb'''
 
     def equaliza(img):
-        #equaliza imagens nos canais R,G,B
+        # equaliza imagens nos canais R,G,B
         b, g, r = cv2.split(img)
         red = cv2.equalizeHist(r)
         green = cv2.equalizeHist(g)
         blue = cv2.equalizeHist(b)
-        blue=b
+        blue = b
         return cv2.merge((blue, green, red))
-    
+
     def readText(img):
         #pytesseract.pytesseract.tesseract_cmd = ( r"C:\Program Files (x86)\Tesseract")
         im = Image.fromarray(img)
